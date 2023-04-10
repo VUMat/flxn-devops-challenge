99ed38f9b7e9a3a2bfba277f895dd44988adc37e
Viachaslau Matsukevich <viachaslau.matsukevich@gmail.com>
2023-04-10 15:12:29 +0200

    update app code to meet flake8 concerns

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import os
 import sqlite3
 from flask import Flask, g
 
+
 # Initialize Flask app
 app = Flask(__name__)
 
+
 # Define the path to the database file
 DATABASE = '/tmp/hello.db'
+
 
 # Define a function to get the database connection
 def get_db():
@@ -16,6 +19,7 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 # Define a function to close the database connection when the app context ends
 @app.teardown_appcontext
 def close_connection(exception):
@@ -23,24 +27,32 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+
 # Define the route for the homepage
 @app.route('/')
 def hello():
     # Get the database connection
     conn = get_db()
+
     # Create a cursor to execute SQL commands
     c = conn.cursor()
+
     # Create the visits table if it doesn't exist
     c.execute("CREATE TABLE IF NOT EXISTS visits (timestamp TEXT)")
+
     # Insert a new visit into the visits table
     c.execute("INSERT INTO visits VALUES (datetime('now'))")
+
     # Commit the changes to the database
     conn.commit()
+
     # Count the total number of visits
     c.execute("SELECT COUNT(*) FROM visits")
     count = c.fetchone()[0]
+
     # Return a response with the total number of visits
     return 'Hello World! I have been visited {} times.'.format(count)
+
 
 # Switch to the non-root user
 if __name__ == '__main__':
